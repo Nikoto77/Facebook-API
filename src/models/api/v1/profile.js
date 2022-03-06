1e5cecfc7e77e949567b44c207640a4fe95e771f
Nicolas Floris <floris.nicolas@outlook.fr>
2022-03-06 20:39:03 +0100

    v4

--- src/models/api/v1/profile.js
+++ src/models/api/v1/profile.js
@@ -2,25 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
-/* export  async function updateProfile(firstName, lastName, userId) {
- return prisma.profile.upsert({
-  where: {
-         userId: userId,
-         },
-         update: {
-          firstName: firstName,
-          lastName: lastName,
-         },
-         create: {
-          firstName,
-          lastName,
-          User: {
-             connect: { id: userId },
-          }
-        } 
-    })
- } */
-
 export async function getProfile(id){
     return prisma.profile.findUnique(
         {
--- src/models/api/v1/users.js
+++ src/models/api/v1/users.js
@@ -2,22 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
-/* export async function registerDto(email,password){
- return prisma.user.create({
-  data:{
-    email,
-    password
-  }
- })
-} */
-/* export async function LoginDto(email,password){
- return prisma.user.findFirst({
-  where: {
-    email : email,
-    password : password
-    }
-  })
-} */
 
 export const findById = ({ id }) =>
   prisma.user.findUnique({
